2fbec7f4d150018ed698490529c9364f9f6aacd8
berkterek <42406263+berkterek@users.noreply.github.com>
2023-12-28 15:35:54 +0300

    feat: sample 11 done enemy factory get enemy prefabs from scriptable object by resources file

---
+++ ZenjectExample/Assets/_GameFolders/Scripts/Concretes/Factories/EnemyPrefabFactory_1.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Zenject;
+using ZenjectExample.Controllers;
+using ZenjectExample.ScriptableObjects;
+
+namespace ZenjectExample.Factories
+{
+    public class EnemyPrefabFactory_1 : IFactory<AiEntityController>
+    {
+        readonly IEnemyPrefabHolder _enemyPrefabHolder;
+        private readonly DiContainer _container;
+
+        public EnemyPrefabFactory_1(DiContainer container, IEnemyPrefabHolder enemyPrefabHolder)
+        {
+            _container = container;
+            _enemyPrefabHolder = enemyPrefabHolder;
+        }
+        
+        public AiEntityController Create()
+        {
+            var enemyPrefab = _enemyPrefabHolder.GetRandomEntity();
+            return _container.InstantiatePrefabForComponent<AiEntityController>(enemyPrefab);
+        }
+    }
+}
---
+++ ZenjectExample/Assets/_GameFolders/Scripts/Concretes/Installers/Sample11MonoInstaller.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using Zenject;
+using ZenjectExample.Abstracts.Controllers;
+using ZenjectExample.Abstracts.Inputs;
+using ZenjectExample.Controllers;
+using ZenjectExample.Factories;
+using ZenjectExample.Inputs;
+using ZenjectExample.ScriptableObjects;
+
+namespace ZenjectExample.Installers
+{
+    public class Sample11MonoInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container.Bind<IInputReader>().To<NewInputReader>().AsSingle().NonLazy();
+            Container.Bind<IPlayerController>().FromComponentInHierarchy().AsSingle().Lazy();
+            Container.Bind<IEnemyPrefabHolder>().To<EnemyPrefabHolderSO>().FromMethod(context => 
+                    LoadScriptableObjectFromResources<EnemyPrefabHolderSO>("EnemyPrefabHolder"))
+                .AsSingle();
+            Container.BindFactory<AiEntityController, EnemyPrefabFactory1PlaceHolder>()
+                .FromFactory<EnemyPrefabFactory_1>();
+            
+            Container.Bind<IMoveFactory>().To<SimpleMoveFactory>().AsSingle().Lazy();
+        }
+        
+        private T LoadScriptableObjectFromResources<T>(string resourcePath) where T : ScriptableObject
+        {
+            return Resources.Load<T>(resourcePath);
+        }
+    }
+}
